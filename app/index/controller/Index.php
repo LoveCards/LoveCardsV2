e02211e22fda713a4517195fb912a5f305b52c1e
zhiguai <2635435377@qq.com>
2023-12-07 00:04:05 +0800

    ✨ feat: 主题自定义页面部分

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -11,82 +11,33 @@ use app\common\Theme;
 use app\index\BaseController;
 use think\facade\Request;
 
+use app\index\controller\Cards;
+
 class Index extends BaseController
 {
-    public function Index()
-    {
-        define("CONST_G_TOP_LISTS_MAX", 32); //置顶卡片列表最大个数
-        define("CONST_G_HOT_LISTS_MAX", 8); //热门卡片列表最大个数
-
-        //Cards置顶列表
-        $var_l_def_result = Db::table('cards')
-            ->where('status', 0)
-            ->where('top', 1)
-            ->order('id', 'desc')
-            ->limit(CONST_G_TOP_LISTS_MAX)
-            ->select()->toArray();
-        $var_l_def_cards_lists = $var_l_def_result;
-        //Cards推荐列表
-        //$result = Db::table('cards')->where('status', 0)->where('top', 0)->order(['good','comment'=>'desc'])
-        //->limit(CONST_G_HOT_LISTS_MAX)->select()->toArray();
-        $var_l_def_result = Db::query("select * from cards where top = '0' and status = '0' order by IF(ISNULL(woName),1,0),comments*0.3+good*0.7 desc limit 0," . CONST_G_HOT_LISTS_MAX);
-        //合并推荐列表到置顶列表
-        $var_l_def_cards_lists = array_merge($var_l_def_cards_lists, $var_l_def_result);
-        //取Good状态合并到CardsList数据
-        for ($i = 0; $i < sizeof($var_l_def_cards_lists); $i++) {
-            $var_l_def_result = Db::table('good')->where('aid', 1)->where('ip', $this->attrGReqIp);
-            //查找对应封面
-            if ($var_l_def_result->where('pid', $var_l_def_cards_lists[$i]['id'])->findOrEmpty() == []) {
-                //未点赞
-                $var_l_def_cards_lists[$i]['ipGood'] = false;
-            } else {
-                //已点赞
-                $var_l_def_cards_lists[$i]['ipGood'] = true;
-            }
-        }
-
-        //Tag列表
-        $var_l_def_result = Db::table('tags')->where('status', 0)->select()->toArray();
-        View::assign([
-            'CardsTagsListJson' => json_encode($var_l_def_result),
-            'CardsTagsList' => $var_l_def_result
-        ]);
-
-        //Cards列表;
-        View::assign('CardsList', $var_l_def_cards_lists);
-
-        //基础变量
-        View::assign([
-            'ViewTitle'  => '推荐',
-        ]);
-
-        //输出模板
-        return View::fetch($this->attrGReqView['html']);
-    }
-
     public function Customize()
     {
         //基础变量
         View::assign([
             'ViewTitle'  => '自定义页面',
         ]);
-
         //模拟
-        $tDef_ControllerName = Request::param('Controller');
+        Request::param('Controller') ? $tDef_ControllerName = Request::param('Controller') : $tDef_ControllerName = 'index';
         Request::param('Action') ? $tDef_ActionName = Request::param('Action') : $tDef_ActionName = 'index';
-
+        //页面路径
         $tDef_ViewControllerPath = dirname(dirname(dirname(dirname(__FILE__)))) . '/public/theme/' . $this->attrGDefNowThemeDirectoryName . '/app/' . $tDef_ControllerName . '/' . $tDef_ActionName;
-
-        //dd(File::read_file($tDef_ViewControllerPath . '.html'));
         //不存在跳转404
         if (!File::read_file($tDef_ViewControllerPath . '.html')) {
             return redirect('/index/404');
         };
-
         //覆盖
         if (File::read_file($tDef_ViewControllerPath . '.js')) {
             $lDef_AssignData['ViewActionJS'] = true;
         };
+
+        //加载主题变量
+        $obj = new Cards();
+        $obj->CardList();
 
         //输出模板
         return View::fetch('app' . '/' . $tDef_ControllerName . '/' . $tDef_ActionName);
