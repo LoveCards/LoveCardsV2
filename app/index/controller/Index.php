3f27f95efac6dfef0431e84d2ffb5bdcd48f09cc
zhiguai <2635435377@qq.com>
2023-12-11 08:57:32 +0800

    ✨ feat: 完善最新主题引擎

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -15,60 +15,109 @@ use app\index\controller\Cards;
 
 class Index extends BaseController
 {
-    public function Customize()
+
+
+    /**
+     * 从访问URL中提取出可能的AppPath
+     *
+     * @param string $tDef_AppPath 主题的APP路径
+     * @return array
+     */
+    protected function mArrayEasyGetUrlAppPath($tDef_AppPath): array
     {
-        //模拟
-        //Request::param('Controller') ? $tDef_ControllerName = Request::param('Controller') : $tDef_ControllerName = 'index';
-        //Request::param('Action') ? $tDef_ActionName = Request::param('Action') : $tDef_ActionName = 'index';
-
-        /**
-         * 从访问URL中提取出可能的AppPath
-         *
-         * @param string $tDef_AppPath 主题的APP路径
-         * @return array
-         */
-        function getUrlAppPath($tDef_AppPath): array
-        {
-            // 修剪根路径
-            $lDef_Url = preg_replace('#' . Request::rootUrl() . '#', '', Request::baseUrl(), 1);
-            $lDef_Parts = explode('/', $lDef_Url);
-            //检查数组长度异常直接弹出
-            if (count($lDef_Parts) >= 100) {
-                return [];
-            }
-
-            // 预估路径
-            $lDef_TrimmedParts = [];
-            $lDef_PartialUrl = '';
-            foreach ($lDef_Parts as $tDef_Key => $tDef_Part) {
-                //略过0位置
-                if ($tDef_Key) {
-                    $lDef_PartialUrl .= '/' . $tDef_Part;
-                }
-                //echo $lDef_PartialUrl . '   ';
-                // 验证html文件路径是否存在
-                if (File::read_file($_SERVER['DOCUMENT_ROOT'] . $tDef_AppPath . $lDef_PartialUrl . '.html')) {
-                    //防止与上层自动绑定Index验证通过路径重复
-                    if (!isset($lDef_TrimmedParts[$tDef_Key - 1]) || $lDef_TrimmedParts[$tDef_Key - 1] != $lDef_PartialUrl) {
-                        $lDef_TrimmedParts[] = $lDef_PartialUrl;
-                    }
-                } else {
-                    //当匹配失败将尝试绑定Index再次验证文件
-                    if ($lDef_PartialUrl != '/' && File::read_file($_SERVER['DOCUMENT_ROOT'] . $tDef_AppPath . $lDef_PartialUrl . '/index' . '.html')) {
-                        $lDef_TrimmedParts[] = $lDef_PartialUrl . '/index';
-                    }
-                };
-            }
-            //当都不存在时尝试绑定Index/index
-            if (count($lDef_TrimmedParts) == 0 && File::read_file($_SERVER['DOCUMENT_ROOT'] . $tDef_AppPath .  '/index/index' . '.html')) {
-                $lDef_TrimmedParts[] = '/index/index';
-            }
-
-            return $lDef_TrimmedParts;
+        // 修剪根路径
+        $lDef_Url = preg_replace('#' . Request::rootUrl() . '#', '', Request::baseUrl(), 1);
+        $lDef_Parts = explode('/', $lDef_Url);
+        //检查数组长度异常直接弹出
+        if (count($lDef_Parts) >= 100) {
+            return [];
         }
 
+        // 预估路径
+        $lDef_TrimmedParts = [];
+        $lDef_PartialUrl = '';
+        foreach ($lDef_Parts as $tDef_Key => $tDef_Part) {
+            //略过0位置
+            if ($tDef_Key) {
+                $lDef_PartialUrl .= '/' . $tDef_Part;
+            }
+            //echo $lDef_PartialUrl . '   ';
+            // 验证html文件路径是否存在
+            if (File::read_file($_SERVER['DOCUMENT_ROOT'] . $tDef_AppPath . $lDef_PartialUrl . '.html')) {
+                //防止与上层自动绑定Index验证通过路径重复
+                if (!isset($lDef_TrimmedParts[$tDef_Key - 1]) || $lDef_TrimmedParts[$tDef_Key - 1] != $lDef_PartialUrl) {
+                    $lDef_TrimmedParts[] = $lDef_PartialUrl;
+                }
+            } else {
+                //当匹配失败将尝试绑定Index再次验证文件
+                if ($lDef_PartialUrl != '/' && File::read_file($_SERVER['DOCUMENT_ROOT'] . $tDef_AppPath . $lDef_PartialUrl . '/index' . '.html')) {
+                    $lDef_TrimmedParts[] = $lDef_PartialUrl . '/index';
+                }
+            };
+        }
+        //当都不存在时尝试绑定Index/index
+        if (count($lDef_TrimmedParts) == 0 && File::read_file($_SERVER['DOCUMENT_ROOT'] . $tDef_AppPath .  '/index/index' . '.html')) {
+            $lDef_TrimmedParts[] = '/index/index';
+        }
+
+        return $lDef_TrimmedParts;
+    }
+
+    /**
+     * 从配置数组中匹配方法与鉴权
+     * @param string $tDef_AppPath
+     * @param array $lDef_ThemeConfig
+     * @return array
+     */
+    protected function mArrayMatchThemeAppConfig($tDef_AppPath, $lDef_ThemeConfig): array
+    {
+        $lDef_ResultArray = [];
+        $tDef_AppPath = strtolower($tDef_AppPath);
+
+        //匹配方法
+        function matchArray($array, $key)
+        {
+            if ($array && isset($array[$key])) {
+                return $array[$key];
+            } else {
+                return false;
+            }
+        }
+
+        //优先匹配文件 匹配到停止
+        if ($lDef_ThemeConfig) {
+            //匹配文件
+            $lDef_ResultArray['PageAuth'] = matchArray($lDef_ThemeConfig['PageAuth'], $tDef_AppPath);
+            $lDef_ResultArray['PageAssignData'] = matchArray($lDef_ThemeConfig['PageAssignData'], $tDef_AppPath);
+            //没有主机匹配目录
+            if ($lDef_ResultArray['PageAuth'] == false || $lDef_ResultArray['PageAssignData'] == false) {
+                $lDef_Parts = explode('/', $tDef_AppPath);
+                foreach ($lDef_Parts as $value) {
+                    array_pop($lDef_Parts);
+                    $tDef_AppFolderPath = implode('/', $lDef_Parts) . '/';
+                    if ($lDef_ResultArray['PageAuth'] == false) {
+                        $lDef_ResultArray['PageAuth'] = matchArray($lDef_ThemeConfig['PageAuth'], $tDef_AppFolderPath);
+                    }
+                    if ($lDef_ResultArray['PageAssignData'] == false) {
+                        $lDef_ResultArray['PageAssignData'] = matchArray($lDef_ThemeConfig['PageAssignData'], $tDef_AppFolderPath);
+                    }
+                    if ($lDef_ResultArray['PageAuth'] != false || $lDef_ResultArray['PageAssignData'] != false) {
+                        break;
+                    }
+                }
+            }
+        }
+        return $lDef_ResultArray;
+    }
+
+    public function Customize()
+    {
+
+        //基础变量
+        //$this->attrGReqAssignArray['ViewTitle']  = '自定义页面';
+
         // 页面路径
-        $tDef_AppPathArray = getUrlAppPath('/theme/' . $this->attrGReqView['Theme']['DirectoryName'] . '/app');
+        $tDef_AppPathArray = $this->mArrayEasyGetUrlAppPath('/theme/' . $this->attrGReqView['Theme']['DirectoryName'] . '/app');
         $tDef_AppPath = end($tDef_AppPathArray);
         // 获取主题匹配JS路径
         $lDef_PageJsPath = $_SERVER['DOCUMENT_ROOT'] . '/theme/' . $this->attrGReqView['Theme']['DirectoryName'] . '/app' . $tDef_AppPath;
@@ -77,60 +126,17 @@ class Index extends BaseController
         };
 
         //dd($tDef_AppPath);
-
         //加载app指定方法 变量
-        function test($tDef_AppPath, $lDef_ThemeConfig)
-        {
-            $lDef_ResultArray = [];
-            $tDef_AppPath = strtolower($tDef_AppPath);
-
-            //匹配方法
-            function matchArray($array, $key)
-            {
-                if ($array && isset($array[$key])) {
-                    return $array[$key];
-                } else {
-                    return false;
-                }
-            }
-
-            //优先匹配文件 匹配到停止
-            if ($lDef_ThemeConfig) {
-                //匹配文件
-                $lDef_ResultArray['PageAuth'] = matchArray($lDef_ThemeConfig['PageAuth'], $tDef_AppPath);
-                $lDef_ResultArray['PageAssignData'] = matchArray($lDef_ThemeConfig['PageAssignData'], $tDef_AppPath);
-                //没有主机匹配目录
-                if ($lDef_ResultArray['PageAuth'] == false || $lDef_ResultArray['PageAssignData'] == false) {
-                    $lDef_Parts = explode('/', $tDef_AppPath);
-                    foreach ($lDef_Parts as $value) {
-                        array_pop($lDef_Parts);
-                        $tDef_AppFolderPath = implode('/', $lDef_Parts) . '/';
-                        if ($lDef_ResultArray['PageAuth'] == false) {
-                            $lDef_ResultArray['PageAuth'] = matchArray($lDef_ThemeConfig['PageAuth'], $tDef_AppFolderPath);
-                        }
-                        if ($lDef_ResultArray['PageAssignData'] == false) {
-                            $lDef_ResultArray['PageAssignData'] = matchArray($lDef_ThemeConfig['PageAssignData'], $tDef_AppFolderPath);
-                        }
-                        if ($lDef_ResultArray['PageAuth'] != false || $lDef_ResultArray['PageAssignData'] != false) {
-                            break;
-                        }
-                    }
-                }
-            }
-            return $lDef_ResultArray;
-        }
         //dd($tDef_AppPath);
-        $ds2 = test($tDef_AppPath, $this->attrGReqView['Theme']['Config']);
-        //dd($ds2);
-        if ($ds2['PageAssignData']) {
-            foreach ($ds2['PageAssignData'] as $key => $value) {
+        $tDef_AppConfigArray = $this->mArrayMatchThemeAppConfig($tDef_AppPath, $this->attrGReqView['Theme']['Config']);
+        //dd($tDef_);
+        if ($tDef_AppConfigArray['PageAssignData']) {
+            foreach ($tDef_AppConfigArray['PageAssignData'] as $key => $value) {
                 $test = new Cards;
                 $test->$value();
             }
         }
 
-        //基础变量
-        $this->attrGReqAssignArray['ViewTitle']  = '自定义页面';
         //分配变量
         View::assign($this->attrGReqAssignArray);
 
