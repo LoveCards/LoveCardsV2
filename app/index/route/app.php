1f29b346f73e5c0385f5d44693aa67dadb07fad7
zhiguai <2635435377@qq.com>
2023-12-12 16:18:33 +0800

    ✨ feat: 完善最新主题引擎

--- app/index/route/app.php
+++ app/index/route/app.php
@@ -5,16 +5,6 @@ use think\facade\Route;
 //404
 Route::rule('/404', '/index/Index/error')->append(['code' => 404]);
 
-//Route::rule('/index$', '/Index/index');
-//Route::rule('/index/index', '/Index/index');
-
-// Route::rule('/cards$', '/Cards/index');
-// Route::rule('/cards/index', '/Cards/index');
-// Route::rule('/cards/card', '/Cards/card');
-// Route::rule('/cards/search', '/Cards/search');
-// Route::rule('/cards/add', '/Cards/add');
-// Route::rule('/cards/tag', '/Cards/tag');
-
 //路由拦截
 Route::get('/[:Level0]/[:Level1]', '/index/Index/Customize');
 
