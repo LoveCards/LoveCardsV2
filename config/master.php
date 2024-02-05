eda509d58ffac518c0ee211e02f0b0332faa9b92
CZG <2635435377@qq.com>
2024-02-06 01:42:27 +0800

    🐎 ci: 优化Github自动化删除的文件

--- config/master.php
+++ config/master.php
@@ -6,7 +6,7 @@ return [
     'Upload' => [
         //最大上传图片大小 单位:M
         'UserImageSize' => env('master.UploadUserImageSize', 2),
-        'UserImageExt' => env('master.UploadUserImageSize', 'jpg,png,gif,webp'),
+        'UserImageExt' => env('master.UploadUserImageExt', 'jpg,png,gif,webp'),
     ],
     'UserAuth' => [
         'Captcha' => env('master.UserAuthCaptcha', false),
