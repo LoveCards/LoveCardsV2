6241f6dc0e31d9541a7845355cda8d250109c005
zhiguai <2635435377@qq.com>
2024-09-19 23:03:08 +0800

    🔧 build: 更新 TP框架到v6.1.5

--- app/common/Common.php
+++ app/common/Common.php
@@ -29,8 +29,8 @@ class Common
         return [
             'Name' => 'LoveCards',
             'Url' => '//lovecards.cn',
-            'VerS' => '2.3.0',
-            'Ver' => '17',
+            'VerS' => '2.3.1',
+            'Ver' => '18',
             'GithubUrl' => '//github.com/LoveCards/LoveCardsV2',
             'QGroupUrl' => '//jq.qq.com/?_wv=1027&k=qM8f2RMg',
             'InstallEnvironment' => [
