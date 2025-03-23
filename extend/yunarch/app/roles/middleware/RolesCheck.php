80ea64e24d0ec2d54fa77eb97fdeeb26e20df8a3
zhiguai <2635435377@qq.com>
2025-03-23 20:25:31 +0800

    🐞 fix: 更新角色检查中间件以兼容旧的校验方式

--- extend/yunarch/app/roles/middleware/RolesCheck.php
+++ extend/yunarch/app/roles/middleware/RolesCheck.php
@@ -17,13 +17,15 @@ class RolesCheck
 
         $jwtData = request()->JwtData;
 
-        //查询用户数据
-        $userData = UsersService::Get($jwtData['uid']);
-        //获取用户分组
-        $user_roles_id = json_decode($userData['data']->roles_id);
-        //验证权限
-        if (!Roles::checkIdBaseUrlPass($user_roles_id)) {
-            return Export::Create(null, 401, '权限不足');
+        if (!array_key_exists('aid', $jwtData)) { //临时 兼容旧的校验方式
+            //查询用户数据
+            $userData = UsersService::Get($jwtData['uid']);
+            //获取用户分组
+            $user_roles_id = json_decode($userData['data']->roles_id);
+            //验证权限
+            if (!Roles::checkIdBaseUrlPass($user_roles_id)) {
+                return Export::Create(null, 401, '权限不足');
+            }
         }
 
         // 传递当前管理员全部数据
