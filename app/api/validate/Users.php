d72e04a0b0a174f74e1782b4c05ac10fd378a2f3
zhiguai <2635435377@qq.com>
2025-04-16 01:07:45 +0800

    ✨ feat: 在用户编辑功能中新增roles_id字段，更新验证规则以支持权限组格式检查

--- app/api/validate/Users.php
+++ app/api/validate/Users.php
@@ -18,30 +18,64 @@ class Users extends Validate
         return true;
     }
 
+    function jsonTypePass($value, $rule)
+    {
+        //可以解析为数组
+        if ($rule === 'array' && !is_array($value)) {
+            return false;
+        }
+        //其他
+        if ($rule === 'integer' && !is_int($value)) {
+            return false;
+        }
+        if ($rule === 'string' && !is_string($value)) {
+            return false;
+        }
+        if ($rule === 'bool' && !is_bool($value)) {
+            return false;
+        }
+        return true;
+    }
+
+    function arrayJson($value)
+    {
+        $decoded = json_decode($value, true);
+        if (json_last_error() === JSON_ERROR_NONE) {
+            return $this->jsonTypePass($decoded, 'array');
+        } else {
+            return false;
+        }
+    }
+
     //定义验证规则
     protected $rule =   [
         'id' => 'number',
-        'email' => 'email|unique:users',
+        'email' => 'length:3,254|email|unique:users',
+        'number' => 'length:3,20|unique:users',
         'phone' => 'mobile|unique:users',
         'username' => 'length:3,12|chsDash|unique:users',
         'password' => 'length:5,36|password',
         'status' => 'number',
+
+        'roles_id' => 'arrayJson',
     ];
 
     //定义错误信息
     protected $message  =   [
-        'id.require' => 'id不得为空',
-        'id.number' => 'id格式错误',
+        'id.require' => 'ID不得为空',
+        'id.number' => 'ID格式错误',
 
         'status.require' => '状态不得为空',
         'status.number' => '状态格式错误',
 
         'number.require' => '账号不得为空',
+        'number.length'     => '账号超出范围(3-20)',
         'number.unique' => '账号已存在',
 
         'email.require' => '邮箱不得为空',
         'email.email' => '邮箱格式不正确',
         'email.unique' => '邮箱已存在',
+        'emil.length'     => '账号超出范围(3-254)',
 
         'phone.require' => '手机号不得为空',
         'phone.mobile' => '手机号格式不正确',
@@ -56,6 +90,7 @@ class Users extends Validate
         'password.length'     => '密码超出范围(5-36)',
         'password.password' => '密码只能为大写、小写、数字或特殊字符',
 
+        'roles_id.arrayJson' => '权限组格式错误',
     ];
 
     //登入场景
@@ -75,7 +110,7 @@ class Users extends Validate
     //编辑场景
     protected function sceneEdit()
     {
-        return $this->only(['id', 'email', 'phone', 'username', 'password', 'status'])
+        return $this->only(['id', 'number', 'roles_id', 'email', 'phone', 'username', 'password', 'status'])
             ->append('id', 'require');
     }
 }
