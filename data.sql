34ea438013587fcb4ad0368be9c249cf6cd4ae6b
jellyfish-p <jellyfish_p@vip.qq.com>
2025-05-17 10:13:34 +0800

    修改sql以兼容低版本的mysql

--- data.sql
+++ data.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- 主机： localhost
--- 生成日期： 2024-12-24 17:49:08
--- 服务器版本： 8.0.36
--- PHP 版本： 8.0.26
+-- 主机：localhost
+-- 生成日期：2024-12-24 17:49:08
+-- 服务器版本：8.0.36
+-- PHP 版本：8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 数据库： `456_chizg_cn`
+-- 数据库：`456_chizg_cn`
 --
 
 -- --------------------------------------------------------
@@ -33,7 +33,7 @@ CREATE TABLE `admin` (
   `password` varchar(64) NOT NULL,
   `power` int NOT NULL DEFAULT '0',
   `uuid` varchar(64) DEFAULT '' COMMENT '登入凭证'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=COMPACT;
 
 --
 -- 转存表中的数据 `admin`
@@ -60,13 +60,13 @@ CREATE TABLE `cards` (
   `good` int NOT NULL DEFAULT '0' COMMENT '点赞数',
   `comments` int NOT NULL DEFAULT '0' COMMENT '评论数',
   `look` int NOT NULL DEFAULT '0' COMMENT '浏览量',
-  `tag` varchar(256) DEFAULT '' COMMENT '标签Json',
+  `tag` varchar(256) DEFAULT '' COMMENT '标签 Json',
   `model` int DEFAULT '0' COMMENT '卡片模式',
   `time` timestamp NOT NULL COMMENT '发布时间',
-  `ip` varchar(256) DEFAULT '' COMMENT '发布IP',
+  `ip` varchar(256) DEFAULT '' COMMENT '发布 IP',
   `top` enum('0','1') DEFAULT '0' COMMENT '置顶状态',
   `status` enum('0','1') DEFAULT '0' COMMENT '封禁状态'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -76,15 +76,15 @@ CREATE TABLE `cards` (
 
 CREATE TABLE `comments` (
   `id` int NOT NULL COMMENT 'pid/aid:2',
-  `aid` int NOT NULL COMMENT '应用ID',
-  `pid` varchar(256) NOT NULL COMMENT '条目ID',
+  `aid` int NOT NULL COMMENT '应用 ID',
+  `pid` varchar(256) NOT NULL COMMENT '条目 ID',
   `uid` int NOT NULL,
   `content` varchar(256) NOT NULL COMMENT '内容',
   `name` varchar(256) NOT NULL COMMENT '我的名字',
-  `ip` varchar(256) NOT NULL COMMENT '发布IP',
+  `ip` varchar(256) NOT NULL COMMENT '发布 IP',
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '发布时间',
   `status` int NOT NULL COMMENT '封禁状态'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=COMPACT;
 
 -- --------------------------------------------------------
 
@@ -94,12 +94,12 @@ CREATE TABLE `comments` (
 
 CREATE TABLE `good` (
   `id` int NOT NULL,
-  `aid` int NOT NULL COMMENT '应用ID',
-  `pid` int NOT NULL COMMENT '条目ID',
+  `aid` int NOT NULL COMMENT '应用 ID',
+  `pid` int NOT NULL COMMENT '条目 ID',
   `uid` int NOT NULL,
-  `ip` varchar(32) NOT NULL COMMENT '发布IP',
+  `ip` varchar(32) NOT NULL COMMENT '发布 IP',
   `time` datetime NOT NULL COMMENT '发布时间'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -109,14 +109,14 @@ CREATE TABLE `good` (
 
 CREATE TABLE `images` (
   `id` int NOT NULL,
-  `aid` int NOT NULL COMMENT '应用ID',
-  `pid` int NOT NULL COMMENT '条目ID',
+  `aid` int NOT NULL COMMENT '应用 ID',
+  `pid` int NOT NULL COMMENT '条目 ID',
   `uid` int NOT NULL,
   `url` varchar(256) NOT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -128,7 +128,7 @@ CREATE TABLE `system` (
   `id` int NOT NULL,
   `name` varchar(255) DEFAULT '',
   `value` varchar(2555) NOT NULL DEFAULT ''
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- 转存表中的数据 `system`
@@ -155,14 +155,14 @@ INSERT INTO `system` (`id`, `name`, `value`) VALUES
 
 CREATE TABLE `tags` (
   `id` int NOT NULL COMMENT 'tid/pid',
-  `aid` int NOT NULL COMMENT '应用ID',
+  `aid` int NOT NULL COMMENT '应用 ID',
   `name` varchar(8) DEFAULT '' COMMENT '标签名',
   `tip` varchar(64) DEFAULT '' COMMENT '提示',
   `status` int NOT NULL DEFAULT '0' COMMENT '封禁状态',
   `deleted_at` datetime DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=COMPACT;
 
 -- --------------------------------------------------------
 
@@ -172,11 +172,11 @@ CREATE TABLE `tags` (
 
 CREATE TABLE `tags_map` (
   `id` int NOT NULL COMMENT 'pid',
-  `aid` int NOT NULL COMMENT '应用ID',
+  `aid` int NOT NULL COMMENT '应用 ID',
   `pid` int DEFAULT NULL COMMENT 'AID[PID]',
   `tid` int DEFAULT NULL COMMENT 'TagID',
   `created_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=COMPACT;
 
 -- --------------------------------------------------------
 
@@ -196,7 +196,7 @@ CREATE TABLE `users` (
   `updated_at` datetime NOT NULL,
   `deleted_at` datetime DEFAULT NULL,
   `status` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=COMPACT;
 
 --
 -- 转储表的索引
@@ -257,59 +257,59 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- 在导出的表使用AUTO_INCREMENT
+-- 在导出的表使用 AUTO_INCREMENT
 --
 
 --
--- 使用表AUTO_INCREMENT `admin`
+-- 使用表 AUTO_INCREMENT `admin`
 --
 ALTER TABLE `admin`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- 使用表AUTO_INCREMENT `cards`
+-- 使用表 AUTO_INCREMENT `cards`
 --
 ALTER TABLE `cards`
   MODIFY `id` int NOT NULL AUTO_INCREMENT COMMENT 'cid/pid/aid:1';
 
 --
--- 使用表AUTO_INCREMENT `comments`
+-- 使用表 AUTO_INCREMENT `comments`
 --
 ALTER TABLE `comments`
   MODIFY `id` int NOT NULL AUTO_INCREMENT COMMENT 'pid/aid:2';
 
 --
--- 使用表AUTO_INCREMENT `good`
+-- 使用表 AUTO_INCREMENT `good`
 --
 ALTER TABLE `good`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
--- 使用表AUTO_INCREMENT `images`
+-- 使用表 AUTO_INCREMENT `images`
 --
 ALTER TABLE `images`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
--- 使用表AUTO_INCREMENT `system`
+-- 使用表 AUTO_INCREMENT `system`
 --
 ALTER TABLE `system`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- 使用表AUTO_INCREMENT `tags`
+-- 使用表 AUTO_INCREMENT `tags`
 --
 ALTER TABLE `tags`
   MODIFY `id` int NOT NULL AUTO_INCREMENT COMMENT 'tid/pid';
 
 --
--- 使用表AUTO_INCREMENT `tags_map`
+-- 使用表 AUTO_INCREMENT `tags_map`
 --
 ALTER TABLE `tags_map`
   MODIFY `id` int NOT NULL AUTO_INCREMENT COMMENT 'pid';
 
 --
--- 使用表AUTO_INCREMENT `users`
+-- 使用表 AUTO_INCREMENT `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
