fa048a60df73b8fa4cc08299c3c055321107d6ad
zhiguai <2635435377@qq.com>
2025-06-25 15:05:00 +0800

    feat: 优化 Comments 模型，添加卡片删除逻辑及相关方法

--- app/api/model/Comments.php
+++ app/api/model/Comments.php
@@ -8,26 +8,31 @@ use think\model\concern\SoftDelete;
 class Comments extends Model
 {
     //开启软删除
-    // use SoftDelete;
-    // protected $deleteTime = 'deleted_at';
+    use SoftDelete;
+    protected $deleteTime = 'deleted_at';
 
     //自动时间戳
-    // protected $autoWriteTimestamp = 'datetime';
-    // protected $createTime = 'created_at';
-    // protected $updateTime = 'updated_at';
+    protected $autoWriteTimestamp = 'datetime';
+    protected $createTime = 'created_at';
+    protected $updateTime = 'updated_at';
 
     // 设置字段信息
     protected $schema = [
-        'id' => 'int',
-        'aid' => 'int',
-        'pid' => 'int',
-        'uid' => 'int',
-        'content' => 'mediumText',
-        'name' => 'string',
-        'ip' => 'string',
-        'time' => 'string',
-        'status' => 'enum'
+        'id' => 'INT',
+        'aid' => 'INT',
+        'pid' => 'INT',
+        'is_top' => 'INT',
+        'status' => 'INT',
+        'user_id' => 'INT',
+        'data' => 'JSON',
+        'content' => 'TEXT',
+        'goods' => 'INT',
+        'post_ip' => 'VARCHAR',
+        'created_at' => 'TIMESTAMP',
+        'updated_at' => 'TIMESTAMP',
+        'deleted_at' => 'TIMESTAMP',
     ];
+
 
     // 默认排除字段
     // protected static $withoutField = [
