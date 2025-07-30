f51e9de69ae9aac63a3931987874324570efb118
zhiguai <2635435377@qq.com>
2025-07-31 01:06:30 +0800

    新增主题管理功能，更新路由和角色权限配置

--- app/api/route/admin.php
+++ app/api/route/admin.php
@@ -1,4 +1,5 @@
 <?php
+
 use think\facade\Route;
 
 use app\api\middleware\JwtAuthCheck;
@@ -7,6 +8,8 @@ use yunarch\app\roles\middleware\RolesCheck;
 
 Route::group('', function () {
     //超管
+    Route::get('system/themes', 'admin.System/themes');
+
     Route::get('system/config', 'admin.System/config');
     Route::post('system/config', 'admin.System/setConfig');
 
