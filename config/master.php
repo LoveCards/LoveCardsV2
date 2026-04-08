ae8018d8c4fe69d203f9aeeaf8cecd9ad0336a52
zhiguai <2635435377@qq.com>
2026-04-08 18:17:38 +0800

    ✨ style: 统一 master.php 格式为 return [] 语法

--- config/master.php
+++ config/master.php
@@ -1,37 +1,31 @@
 <?php
 
-return array (
-  'System' => 
-  array (
-    'ThemeDirectory' => 'index',
-    'VisitorMode' => true,
-  ),
-  'Upload' => 
-  array (
-    'UserImageSize' => 2,
-    'UserImageExt' => 'jpg,png,gif,webp,jpeg',
-  ),
-  'UserAuth' => 
-  array (
-    'Captcha' => false,
-  ),
-  'Cards' => 
-  array (
-    'PictureLimit' => 10,
-    'TagLimit' => 3,
-    'Approve' => true,
-    'ImageSize' => 3,
-    'CommentsStatus' => true,
-  ),
-  'Comments' => 
-  array (
-    'Approve' => true,
-    'PictureLimit' => 9,
-  ),
-  'Geetest' => 
-  array (
-    'Status' => false,
-    'Id' => '145e5424cb89698be0c58a1060483735',
-    'Key' => 'e4a515f644b3e14c20d11dc90d326743',
-  ),
-);
+return [
+    'System' => [
+        'ThemeDirectory' => 'index',
+        'VisitorMode' => true,
+    ],
+    'Upload' => [
+        'UserImageSize' => 2,
+        'UserImageExt' => 'jpg,png,gif,webp,jpeg',
+    ],
+    'UserAuth' => [
+        'Captcha' => false,
+    ],
+    'Cards' => [
+        'PictureLimit' => 10,
+        'TagLimit' => 3,
+        'Approve' => true,
+        'ImageSize' => 3,
+        'CommentsStatus' => true,
+    ],
+    'Comments' => [
+        'Approve' => true,
+        'PictureLimit' => 9,
+    ],
+    'Geetest' => [
+        'Status' => false,
+        'Id' => '145e5424cb89698be0c58a1060483735',
+        'Key' => 'e4a515f644b3e14c20d11dc90d326743',
+    ],
+];
